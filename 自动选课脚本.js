15018e42876d3b21884f53726f3ead7ab7da86c3
xiaofei <122727418+xiaozhou26@users.noreply.github.com>
2023-12-20 21:11:54 +0800

    Update 自动选课脚本.js

--- 自动选课脚本.js
+++ 自动选课脚本.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/lyy0709/select_course
 // @version      1.2
 // @description  在输入框中输入需要选择的课程点击开始选课即可抢课（确保已经搜索到课程）
-// @author       lyy0709
+// @author       lyy0709,xiaozhou26
 // @match        https://bk.cup.edu.cn/course-selection/*
 // @grant        none
 // ==/UserScript==
@@ -13,19 +13,23 @@
 
     let interval;
 
-    // 创建输入框和按钮
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.placeholder = '输入课程名称';
+    function createInputAndButton() {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.placeholder = '输入课程名称';
 
-    const button = document.createElement('button');
-    button.textContent = '开始选课';
+        const button = document.createElement('button');
+        button.textContent = '开始选课';
 
-    document.body.prepend(button);
-    document.body.prepend(input);
+        document.body.prepend(button);
+        document.body.prepend(input);
+
+        return { input, button };
+    }
+
+    const { input, button } = createInputAndButton();
 
     button.addEventListener('click', () => {
-        // 获取输入的课程名称
         const targetCourseName = input.value.trim();
         if (!targetCourseName) {
             alert('请输入课程名称');
@@ -33,30 +37,24 @@
         }
 
         interval = setInterval(() => {
-            // 获取所有课程名称元素
             const courseElements = document.querySelectorAll('.course-name');
 
-            // 找到目标课程名称对应的元素
-            let selectButton;
-            courseElements.forEach(course => {
-                if (course.textContent.includes(targetCourseName)) {
-                    // 获取对应的选课按钮
-                    selectButton = course.closest('tr').querySelector('button.course-select');
-                }
-            });
+            const targetElement = Array.from(courseElements).find(course => course.textContent.includes(targetCourseName));
 
-            if (selectButton) {
-                // 点击选课按钮
+            if (targetElement) {
+                const selectButton = targetElement.closest('tr').querySelector('button.course-select');
                 selectButton.click();
                 console.log("已尝试选课");
-                // 检查按钮文本是否变为“退课”
+
                 if (selectButton.textContent.includes('退课')) {
                     console.log("选课成功，停止选课");
                     clearInterval(interval);
+                    input.disabled = true;
+                    button.disabled = true;
                 }
             }
 
-        }, 2000); // 每隔2000毫秒（2秒）执行一次
+        }, 2000);
     });
 
 })();
